6d393ab9e2ea4d1b62f877ddcb2eb7a44f5e16c9
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2022-01-27 00:14:57 +0200

    temp v. 0.0.1

---
+++ pages/works.js
@@ -0,0 +1,44 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react';
+import Layout from '../components/layouts/article';
+import Section from '../components/section';
+import { WorkGridItem } from '../components/grid-item';
+
+import thumbParser from '../public/images/works/parser.png';
+import thumbNotFound from '../public/images/works/notFound.png';
+
+const Works = () => (
+  <Layout title="Works">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 1]} gap={6}>
+        <Section>
+          <WorkGridItem id="notFound" title="Not Found" thumbnail={thumbNotFound}>
+            NotFound
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+
+      <Section delay={0.4}>
+        <Divider my={6} />
+
+        <Heading as="h3" fontSize={20} mb={4}>
+          Old works
+        </Heading>
+      </Section>
+
+      <SimpleGrid columns={[1, 1, 1]} gap={6}>
+        <Section delay={0.5}>
+          <WorkGridItem id="pythonParcer" thumbnail={thumbParser} title="Universal parser">
+            Async IO Parser in Python
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+  </Layout>
+)
+
+export default Works;
+export { getServerSideProps } from '../components/chakra';
